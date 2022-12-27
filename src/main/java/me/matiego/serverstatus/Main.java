0c6729c558e4cb777f500a79b77b6063c145e379
Matiego <filipowicz.mati@gmail.com>
2022-12-27 12:07:09 +0100

    increased the frequency of checking

--- src/main/java/me/matiego/serverstatus/Main.java
+++ src/main/java/me/matiego/serverstatus/Main.java
@@ -34,7 +34,7 @@ public final class Main extends JavaPlugin {
             if (current.equals(last)) return;
             last = current;
             current.sendWebhook(getConfig().getString("webhook-url", ""));
-        }, 200, 6000);
+        }, 200, 3600);
     }
 
     @Override
